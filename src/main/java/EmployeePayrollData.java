c6ab2f560bd07103f3ce1663edac6e2b80ddd207
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-20 14:25:53 +0530

    Added: retrieve the Employee

---
+++ src/main/java/EmployeePayrollData.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+import java.util.Objects;
+
+/*
+Making a class of EmployeePayrollData So we can add id,name,salary items in a arrayList using constructor of
+EmployeePayrollData
+ */
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+    public LocalDate startDate;
+
+    public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollData{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", date=" + startDate +
+                '}';
+    }
+
+
+}
+
--- src/test/java/EmployeePayrollTest.java
+++ src/test/java/EmployeePayrollTest.java
@@ -1,6 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeePayrollTest {
 
+    @Test
+    /*
+    TestCase to check number of employees in the sql table by using readEmployeePayrollData method
+    and save number of employee in arraylist of employeePayrollData type
+     */
+    public void givenEmpPayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayroll employeePayroll = new EmployeePayroll();
+        List<EmployeePayrollData> employeePayrollData = employeePayroll.readEmployeePayrollData();
+        Assertions.assertEquals(3, employeePayrollData.size());
+
+    }
 
 }
