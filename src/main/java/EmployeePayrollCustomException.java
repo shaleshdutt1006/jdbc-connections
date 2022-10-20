038ec88483acc3b8710e1a919b7659b287baf10d
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-20 23:02:07 +0530

    Added: Retrieving all employees who joined in a particular date range

--- src/main/java/EmployeePayrollCustomException.java
+++
@@ -1,6 +0,0 @@
-public class EmployeePayrollCustomException extends Exception {
-
-    public EmployeePayrollCustomException(String message) {
-        super(message);
-    }
-}
--- src/test/java/EmployeePayrollTest.java
+++ src/test/java/EmployeePayrollTest.java
@@ -9,31 +9,14 @@ class EmployeePayrollTest {
 
     @Test
     /*
-    TestCase to check number of employees in the sql table by using readEmployeePayrollData method
+    TestCase to check number of employees in Particular dat range in sql table by using readEmployeePayrollData method
     and save number of employee in arrayList of employeePayrollData type
      */
-    public void givenEmpPayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollCustomException {
+
+    public void givenEmpPayrollDB_WhenRetrieved_ShouldGiveEmployeeInDataRange() {
         EmployeePayroll employeePayroll = new EmployeePayroll();
         List<EmployeePayrollData> employeePayrollData = employeePayroll.readEmployeePayrollData();
-        Assertions.assertEquals(3, employeePayrollData.size());
-
-    }
-
-    /*
-    TestCase to check salary of employee is updated in the sql table by using readEmployeePayrollData method
-    and save number of employee in arrayList of employeePayrollData type
-     */
-    @Test
-    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDb() throws EmployeePayrollCustomException {
-
-        EmployeePayroll employeePayroll = new EmployeePayroll();
-/*
-Saving the updated salary into the result arrayList and the read data of the employeePayroll in
-expectedEmployeePayrollData
- */
-        List<EmployeePayrollData> ExpectedEmployeePayroll = employeePayroll.readEmployeePayrollData();
-        List<EmployeePayrollData> result = employeePayroll.updateSalary("Teresa", 300000);
-        Assertions.assertEquals(ExpectedEmployeePayroll, result);
+        Assertions.assertEquals(2, employeePayrollData.size());
     }
 
 }
